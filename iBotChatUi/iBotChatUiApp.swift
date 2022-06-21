39cc1707ea4ab2c4e2c23c2db397d623c62e43fb
Caio Castro <caio.castro3909@gmail.com>
2022-06-21 14:13:12 +0200

    Initial Commit

---
+++ iBotChatUi/iBotChatUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBotChatUiApp.swift
+//  iBotChatUi
+//
+//  Created by Caio Castro on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iBotChatUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
